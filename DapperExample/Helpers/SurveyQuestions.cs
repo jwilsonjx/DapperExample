3afbd7432ae327be7f9ffd3cd6cc11f30ef3aca2
Jason Wilson <jwilsonjx@outlook.com>
2017-05-10 13:07:14 -0400

    Add project files.

---
+++ DapperExample/Helpers/SurveyQuestions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperExample.Helpers
+{
+    public class SurveyQuestions
+    {
+        public int question_recid { get; set; }
+        public int company_id { get; set; }
+        public string question1 { get; set; }
+        public string question2 { get; set; }
+        public string question3 { get; set; }
+        public string question4 { get; set; }
+        public string question5 { get; set; }
+
+    }
+}
---
+++ DapperExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DapperExample.Helpers;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DapperExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            List<SurveyQuestions> questions = p.ReadAll();
+        }
+
+        public List<SurveyQuestions> ReadAll()
+        {
+            string connectionString = "Data Source=myDatabase;Initial Catalog=myCatalog;Persist Security Info=True;User ID=userid;Password=password";
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<SurveyQuestions>
+                    ("SELECT * FROM survey_questions (NOLOCK)").ToList();
+            }          
+
+        }
+    }
+}
